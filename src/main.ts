a379b0ef7ebeb8d78d834c0fe32f2c7faeb58a7e
thiendanganncc <thiendangan@ncc.asia>
2023-07-04 19:52:16 +0700

    update accept url

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: '*',
-        credentials: true
-    });
+    app.enableCors(
+        { 
+          origin: ['http://127.0.0.1:3000, https://localhost:3000, https://art-shop-fe.vercel.app/'],
+          methods: ['POST', 'PUT', 'DELETE', 'GET']
+        }
+      );
     await app.listen(5000);
     if (module.hot) {
         module.hot.accept();
