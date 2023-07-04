b2b628f33bc6ba14919029ceaa0275235a883c9d
thiendanganncc <thiendangan@ncc.asia>
2023-07-04 21:24:05 +0700

    update uri

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,13 @@ declare const module: any;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.enableCors({
-        origin: [
-            'http://127.0.0.1:3000',
-            'https://localhost:3000',
-            'https://art-shop-fe.vercel.app',
-            'http://art-shop-fe.vercel.app',
-            'https://ec2-34-198-71-168.compute-1.amazonaws.com',
-            'https://ec2-34-198-71-168.compute-1.amazonaws.com:8000'
-        ],
-        methods: ['GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH'],
-    });
+    app.enableCors(
+        { 
+          origin: ['http://127.0.0.1:3000', 'https://localhost:3000', 'https://art-shop-fe.vercel.app/', 'http://art-shop-fe.vercel.app', 'http://art-shop-fe.vercel.app', 'https://ec2-34-198-71-168.compute-1.amazonaws.com', 'https://ec2-34-198-71-168.compute-1.amazonaws.com:8000'],
+          methods: ['POST', 'PUT', 'DELETE', 'GET', 'OPTIONS'],
+          
+        }
+      );
     await app.listen(5000);
     if (module.hot) {
         module.hot.accept();
