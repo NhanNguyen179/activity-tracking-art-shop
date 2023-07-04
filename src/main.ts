929684391e327f581de01af646350a619fcee04e
thiendanganncc <thiendangan@ncc.asia>
2023-07-04 20:32:06 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors(
         { 
-          origin: ['http://127.0.0.1:3000', 'https://localhost:3000', 'https://art-shop-fe.vercel.app/', 'http://art-shop-fe.vercel.app/', 'http://art-shop-fe.vercel.app/', 'https://ec2-34-198-71-168.compute-1.amazonaws.com', 'https://ec2-34-198-71-168.compute-1.amazonaws.com:8000'],
+          origin: ['http://127.0.0.1:3000', 'https://localhost:3000', 'https://art-shop-fe.vercel.app/', 'http://art-shop-fe.vercel.app', 'http://art-shop-fe.vercel.app', 'https://ec2-34-198-71-168.compute-1.amazonaws.com', 'https://ec2-34-198-71-168.compute-1.amazonaws.com:8000'],
           methods: ['POST', 'PUT', 'DELETE', 'GET']
         }
       );
